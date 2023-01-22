156b89400d3f059abc8eea2a450ca69d3d72a9d5
MatheusSouzaz <123311611+MatheusSouzaz@users.noreply.github.com>
2023-01-22 11:50:25 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,20 @@
+
+ console.log('mostrar o document', document);
+ 
+ console.log(document.querySelector(".botao-trailer"));
+
+ const botaoTrailer =document.querySelector('.botao-trailer');
+ const video =document.getElementById("video")
+ const modal = document.querySelector(".modal");
+ const botaoFecharModal = document.querySelector(".fechar-modal");
+const linkDoVideo = video.src;
+
+ botaoTrailer.addEventListener("click", ()=> {modal.classList.toggle("aberto")
+ video.setAttribute("src", linkDoVideo)
+});
+
+botaoFecharModal.addEventListener("click", ()=>{
+    modal.classList.toggle("aberto");
+    video.setAttribute("src", "")
+});
+
